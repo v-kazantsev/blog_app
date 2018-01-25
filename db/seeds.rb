bcbdb047fa1a50cf21268902d9bfef5d74f9b4fd
v-kazantsev <vvs201@mail.ru>
2018-01-25 13:03:44 +1000

    Added blogs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |i|
+  Blog.create!(
+    title: "Blog No #{i+1}",
+    source: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque lorem ex, sodales eu nisi nec, fringilla tempor sapien. Nullam pulvinar sodales dolor, et elementum tellus. ",
+    body: "Cras eget arcu pulvinar turpis posuere eleifend a quis tellus. Pellentesque euismod non enim eu aliquet. Duis ultrices sed arcu ac faucibus. Nulla malesuada lorem mauris, a placerat lacus laoreet ultricies. Quisque vel mattis elit. Maecenas condimentum vel enim et fringilla. Pellentesque pellentesque, enim eu imperdiet pharetra, metus eros auctor diam, a ullamcorper magna turpis malesuada ipsum. Integer ut dictum magna. Pellentesque ac est nibh. Aliquam nec pretium orci. Mauris porttitor tortor tellus. Donec dapibus faucibus luctus. Morbi ligula mauris, sodales sit amet dapibus eget, malesuada ut lorem. Nam eget tristique arcu."
+  )
+end
